965d9ee0d1360cff3dd22bc121e4b87935e743ba
IlCarretto <carlorebuffat412@gmail.com>
2022-12-12 16:05:05 +0100

    fix

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ require_once __DIR__ . "/Models/Prodotto.php";
 require_once __DIR__ . "/Models/Categoria.php";
 
 $categories = [
-    new Category("Cane"),
-    new Category("Gatto")
+    new Categoria("Cane"),
+    new Categoria("Gatto")
 ]
 
-$cibo = new Prodotto("croccantini.jpg", "Croccantini", 12.99, "Monge", "Cibo", new Category("Cane"));
+$cibo = new Prodotto("croccantini.jpg", "Croccantini", 12.99, "Monge", "Cibo", new Categoria("Cane"));
 var_dump($cibo);
